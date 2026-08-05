f7387afb0464ee0e741041b3d86b62dd29ae005d
Simple-Logz <jobsforjohn10@gmail.com>
2026-08-05 17:49:05 -0400

    Delete MegaProz-Consult directory

--- MegaProz-Consult/infra/outputs.tf
+++
@@ -1,2 +0,0 @@
-output "api_url" { value = "https://${aws_lb.main.dns_name}" }
-output "db_endpoint" { value = aws_db_instance.main.endpoint }
--- MegaProz-Consult/infra/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "app_name" { default = "MegaProz-Consult" }
-variable "aws_region" { default = "us-east-1" }
-variable "db_password" { sensitive = true }
-variable "image_tag" { default = "latest" }
